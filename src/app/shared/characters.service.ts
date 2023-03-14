f2c4b3bb09c243517a1ace8897e08985807864a2
NShevch <86680685+NShevch@users.noreply.github.com>
2023-03-14 17:27:50 +0200

    done: http | charactersService

--- src/app/shared/characters.service.ts
+++ src/app/shared/characters.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 import { Character } from './character.model';
 
 @Injectable({
@@ -20,13 +21,12 @@ export class CharactersService {
     this.charactersFullList = list;
   }
 
-  getData() {
-    return this.http.get('https://rickandmortyapi.com/api/character').pipe(
+  getAllCharacters(url: string, dataSoFar: Character[] = []): Observable<Character[]> {
+    if (!url) return of(dataSoFar);
+    return this.http.get(url).pipe(
       map((data: any) => {
         let charactersList = data.results;
-        console.log('charactersList')
-        console.log(charactersList)
-        return charactersList.map((character: any) => {
+        const charactersListPrepared = charactersList.map((character: any) => {
           return new Character({
             id: character.id,
             name: character.name,
@@ -37,9 +37,40 @@ export class CharactersService {
             origin: character.origin.name,
             type: character.type || "Unknown", */
           });
-        });
-    }));
+        })
+        dataSoFar = dataSoFar.concat(charactersListPrepared);
+
+        return data;
+      }),
+      switchMap((data: any) => {
+        return this.getAllCharacters(data.info.next, dataSoFar)
+      })
+    );
   }
+
+  getData() {
+    return this.getAllCharacters('https://rickandmortyapi.com/api/character');
+  }
+
+  // getData() {
+  //   return this.http.get('https://rickandmortyapi.com/api/character?page=42').pipe(
+  //     map((data: any) => {
+  //       let charactersList = data.results;
+  //       return charactersList.map((character: any) => {
+  //         return new Character({
+  //           id: character.id,
+  //           name: character.name,
+  //           species: character.species,
+  //           image: character.image/* ,
+  //           gender: character.gender,
+  //           status: character.status,
+  //           origin: character.origin.name,
+  //           type: character.type || "Unknown", */
+  //         });
+  //       });
+  //   }));
+  // }
+
 
   getSpecificCharacter(id: number) {
     console.log('getSpecificCharacter')
